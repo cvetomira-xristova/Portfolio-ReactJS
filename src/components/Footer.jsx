0e384d0afdbfe59a48e96052db012b1ab9b31bf7
Tsvetomira Hristova <cvetomira.xristova@gmail.com>
2023-03-27 17:12:23 +0300

    Added repo link to the footer;

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import { Typography } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import StayCurrentPortraitIcon from '@mui/icons-material/StayCurrentPortrait';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const lnLink = 'https://www.linkedin.com/in/tsvetomira-hristova-1b9759172/';
 const emailAddress = 'cvetomira.xristova@gmail.com';
+const repoLink = 'https://github.com/cvetomira-xristova/Portfolio-ReactJS';
 
 const socials = [
   { id: 'phone', info: '+359 88 228 1240', icon: <StayCurrentPortraitIcon /> },
@@ -20,6 +22,12 @@ const socials = [
     info: 'Tsvetomira Hristova',
     icon: <LinkedInIcon />,
     link: lnLink,
+  },
+  {
+    id: 'github',
+    info: 'View this repo on Github',
+    icon: <GitHubIcon />,
+    link: repoLink,
   },
 ];
 
